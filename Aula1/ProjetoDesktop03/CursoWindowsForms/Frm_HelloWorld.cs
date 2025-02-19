dde1751258a7dd25c454191c1a10e97932ea4746
fhalves <fhalves@live.com>
2025-02-19 00:23:42 -0300

    UploadProjeto03
    UploadProjeto03

---
+++ Aula1/ProjetoDesktop03/CursoWindowsForms/Frm_HelloWorld.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class Frm_HelloWorld : Form
+    {
+        public Frm_HelloWorld()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Sair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Btn_ModificaLabel_Click(object sender, EventArgs e)
+        {
+            lbl_Titulo.Text = Txt_ConteudoLabel.Text;
+
+            if (lbl_Titulo.Text == "1") {
+                lbl_Titulo.Text = "Assis";
+            }           
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lbl_Titulo.Text = Txt_ConteudoLabel.Text;
+
+            //if (lbl_Titulo.Text == "1")
+            //{
+            //    lbl_Titulo.Text = "ASSIS";
+            //}
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lbl_Titulo.Text = Txt_ConteudoLabel.Text;
+
+        }
+        private void Txt_ConteudoLabel_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void lbl_Titulo_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Frm_HelloWorld_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
